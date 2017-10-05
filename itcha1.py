ac9656a5c5ca46483730c534a704eed86dcc859f
GatoY <derakhy@gmail.com>
2017-10-05 23:36:17 +1100

    good change

--- itcha1.py
+++ itcha1.py
@@ -8,7 +8,7 @@ def text_reply(msg):
 @itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO])
 def download_files(msg):
     msg['Text'](msg['FileName'])
-    return '@%s@%s' % ({'Picture': 'img', 'Video': 'vid'}.get(msg['Type'], 'fil'), msg['FileName'])
+    itchat.send('@%s@%s' % ({'Picture': 'img', 'Video': 'vid'}.get(msg['Type'], 'fil'), msg['FileName']),toUserName='filehelper')
 
 @itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO],isGroupChat = True)
 def download_files(msg):
